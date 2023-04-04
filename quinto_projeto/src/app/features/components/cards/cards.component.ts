f326ade56c62f251b6cedc44d8162d305348419f
Brenda Machado <brenda16machado.bm@gmail.com>
2023-04-03 23:15:26 -0300

    atualização

--- quinto_projeto/src/app/features/components/cards/cards.component.ts
+++ quinto_projeto/src/app/features/components/cards/cards.component.ts
@@ -9,6 +9,8 @@ import { card } from './models/produtos.modulos';
 export class CardsComponent {
 titulo = "Novidades";
 
+exibeProdutos = false;
+
 produtos: Array<card> =[
   {nome: "codigo",
     imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQL9JbWkre6l4qrZhYdoSw9jrJLGxQ9S9HC6PRXs-Zg&s",
@@ -17,8 +19,19 @@ produtos: Array<card> =[
     {nome: "codigo",
     imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQL9JbWkre6l4qrZhYdoSw9jrJLGxQ9S9HC6PRXs-Zg&s",
     sinopse: "kkwkwwkwk",
-    preco : 4000}
+    preco : 4000},
+    {nome: "codigo",
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQL9JbWkre6l4qrZhYdoSw9jrJLGxQ9S9HC6PRXs-Zg&s",
+    sinopse: "kkwkwwkwk",
+    preco : 4000},
+    {nome: "codigo",
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQL9JbWkre6l4qrZhYdoSw9jrJLGxQ9S9HC6PRXs-Zg&s",
+    sinopse: "kkwkwwkwk",
+    preco : 4000},
   ]
-     
+    exibeprodutosim(){
+this.exibeProdutos = !this.exibeProdutos;
+
+    } 
 
 }
