60a27b9facbb91d3fed9b3658f69a4e297145e3e
Brenda Machado <brenda16machado.bm@gmail.com>
2023-04-04 00:21:35 -0300

    ajustando os cards

--- quinto_projeto/src/app/features/components/cards/cards.component.ts
+++ quinto_projeto/src/app/features/components/cards/cards.component.ts
@@ -28,6 +28,22 @@ produtos: Array<card> =[
     imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQL9JbWkre6l4qrZhYdoSw9jrJLGxQ9S9HC6PRXs-Zg&s",
     sinopse: "kkwkwwkwk",
     preco : 4000},
+    {nome: "codigo",
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQL9JbWkre6l4qrZhYdoSw9jrJLGxQ9S9HC6PRXs-Zg&s",
+    sinopse: "kkwkwwkwk",
+    preco : 4000},
+    {nome: "codigo",
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQL9JbWkre6l4qrZhYdoSw9jrJLGxQ9S9HC6PRXs-Zg&s",
+    sinopse: "kkwkwwkwk",
+    preco : 4000},
+    {nome: "codigo",
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQL9JbWkre6l4qrZhYdoSw9jrJLGxQ9S9HC6PRXs-Zg&s",
+    sinopse: "kkwkwwkwk",
+    preco : 4000},
+    {nome: "codigo",
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQL9JbWkre6l4qrZhYdoSw9jrJLGxQ9S9HC6PRXs-Zg&s",
+    sinopse: "kkwkwwkwk",
+    preco : 4000}
   ]
     exibeprodutosim(){
 this.exibeProdutos = !this.exibeProdutos;
